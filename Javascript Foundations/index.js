97215d04d709003fb069154bf00aeb36492d208a
Moses <asugam9@gmail.com>
2024-11-30 10:31:50 +0300

    reverse

---
+++ Javascript Foundations/index.js
@@ -0,0 +1,18 @@
+//String reversal
+
+function stringreverse(str){
+    return str.split('').reverse().join('');
+}
+
+//Count Characters
+
+function countChar(str){
+    return str.length;
+}
+
+//Capitalize Words
+
+function capitalWOrds(str){
+    
+}
+
